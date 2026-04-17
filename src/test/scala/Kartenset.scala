eee2878f7e121fdf0f9f9834172f0a696d87e839
Hanna stanyak <132809702+stanyaha233@users.noreply.github.com>
2026-04-17 09:05:27 +0200

    Update KarteZiehen.scala

--- src/test/scala/Kartenset.scala
+++ src/test/scala/Kartenset.scala
@@ -6,6 +6,11 @@ import org.scalatest.matchers.should.Matchers
 class KartensetSpec extends AnyWordSpec with Matchers {
 
   "A Hand" should {
+    
+    "should start with zero cards" in {
+      val hand = new Hand(List.empty)
+      hand.anzahl shouldBe 0
+    }
 
     "add a card correctly and update the count" in {
       val card = Karte(Farbe.Rot, Zahl.zero)
