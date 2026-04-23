a93d8f8dbba381947a1903e5fa5c88de70aa0ef1
chrncln-create <nicolina.hoffmann@gmail.com>
2026-04-23 18:29:40 +0200

    Update Main.scala

--- src/test/scala/Main.scala
+++ src/test/scala/Main.scala
@@ -6,51 +6,51 @@ import org.scalatest.matchers.should.Matchers
 class KarteSpec extends AnyFlatSpec with Matchers {
 
   "A Karte" should "properly store its color and value" in {
-    val card = Karte(Farbe.Rot, Zahl.sieben)
-    card.farbe should be(Farbe.Rot)
-    card.wert should be(Zahl.sieben)
+    val card = Card(Colour.Red, Number.seven)
+    card.colour should be(Colour.Red)
+    card.value should be(Number.seven)
   }
 
   "Move logic" should "allow a move if the color matches" in {
-    val topCard = Karte(Farbe.Rot, Zahl.sieben)
-    val handCard = Karte(Farbe.Rot, Zahl.drei)
+    val topCard = Card(Colour.Red, Number.seven)
+    val handCard = Card(Colour.Red, Number.three)
 
-    val isPossible = handCard.farbe == topCard.farbe || handCard.wert == topCard.wert
+    val isPossible = handCard.colour == topCard.colour || handCard.value == topCard.value
     isPossible should be(true)
   }
 
   it should "allow a move if the value matches" in {
-    val topCard = Karte(Farbe.Rot, Zahl.sieben)
-    val handCard = Karte(Farbe.Blau, Zahl.sieben)
+    val topCard = Card(Colour.Red, Number.seven)
+    val handCard = Card(Colour.Blue, Number.seven)
 
-    val isPossible = handCard.farbe == topCard.farbe || handCard.wert == topCard.wert
+    val isPossible = handCard.colour == topCard.colour || handCard.value == topCard.value
     isPossible should be(true)
   }
 
   it should "deny a move if neither color nor value matches" in {
-    val topCard = Karte(Farbe.Rot, Zahl.sieben)
-    val handCard = Karte(Farbe.Blau, Zahl.drei)
+    val topCard = Card(Colour.Red, Number.seven)
+    val handCard = Card(Colour.Blue, Number.three)
 
-    val isPossible = handCard.farbe == topCard.farbe || handCard.wert == topCard.wert
+    val isPossible = handCard.colour == topCard.colour || handCard.value == topCard.value
     isPossible should be(false)
   }
 
   it should "detect at least one playable card in a hand" in {
-    val topCard = Karte(Farbe.Rot, Zahl.sieben)
+    val topCard = Card(Colour.Red, Number.seven)
     val hand = List(
-      Karte(Farbe.Blau, Zahl.drei),
-      Karte(Farbe.Gruen, Zahl.Skip),
-      Karte(Farbe.Rot, Zahl.zwei)
+      Card(Colour.Blue, Number.three),
+      Card(Colour.Green, Number.Skip),
+      Card(Colour.Red, Number.two)
     )
 
-    val possible = hand.exists(k => k.farbe == topCard.farbe || k.wert == topCard.wert)
+    val possible = hand.exists(k => k.colour == topCard.colour || k.value == topCard.value)
     possible should be(true)
   }
 
   it should "handle special black cards correctly (Wild cards)" in {
-    val topCard = Karte(Farbe.Rot, Zahl.sieben)
-    val wildCard = Karte(Farbe.Schwarz, Zahl.wahl)
-    val possible = wildCard.farbe == topCard.farbe || wildCard.wert == topCard.wert
+    val topCard = Card(Colour.Red, Number.seven)
+    val wildCard = Card(Colour.Black, Number.choice)
+    val possible = wildCard.colour == topCard.colour || wildCard.value == topCard.value
 
     possible should be(false)
   }
