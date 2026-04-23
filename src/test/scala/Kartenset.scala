c54c77d4d2a6113bccd2de0833595a9c504d496b
chrncln-create <nicolina.hoffmann@gmail.com>
2026-04-23 18:29:44 +0200

    Update Kartenset.scala

--- src/test/scala/Kartenset.scala
+++ src/test/scala/Kartenset.scala
@@ -9,72 +9,72 @@ class KartensetSpec extends AnyWordSpec with Matchers {
     
     "start with zero cards" in {
       val hand = new Hand(List.empty)
-      hand.anzahl shouldBe 0
+      hand.count shouldBe 0
     }
 
     "add a card correctly and update the count" in {
-      val card = Karte(Farbe.Rot, Zahl.zero)
+      val card = Card(Colour.Red, Number.zero)
       val hand = new Hand(List.empty).add(card)
 
-      hand.karten should contain (card)
-      hand.anzahl shouldBe 1
+      hand.cards should contain (card)
+      hand.count shouldBe 1
     }
 
     "maintain all added cards in the order they were added" in {
-      val card1 = Karte(Farbe.Rot, Zahl.zero)
-      val card2 = Karte(Farbe.Schwarz, Zahl.plus4)
+      val card1 = Card(Colour.Red, Number.zero)
+      val card2 = Card(Colour.Black, Number.plus4)
 
       val hand = new Hand(List.empty)
         .add(card1)
         .add(card2)
 
-      hand.karten shouldBe List(card1, card2)
-      hand.anzahl shouldBe 2
+      hand.cards shouldBe List(card1, card2)
+      hand.count shouldBe 2
     }
 
     "be immutable (return a new Hand instance when adding cards)" in {
       val initialHand = new Hand(List.empty)
-      val newHand = initialHand.add(Karte(Farbe.Blau, Zahl.fuenf))
+      val newHand = initialHand.add(Card(Colour.Blue, Number.five))
 
-      initialHand.anzahl shouldBe 0
-      newHand.anzahl shouldBe 1
+      initialHand.count shouldBe 0
+      newHand.count shouldBe 1
     }
 
     // --- Neue Tests für die moeglich-Methode ---
 
     "allow playing a card if the color matches" in {
-      val hand = new Hand(List(Karte(Farbe.Rot, Zahl.drei)))
-      val mitte = Karte(Farbe.Rot, Zahl.acht)
+      val hand = new Hand(List(Card(Colour.Red, Number.three)))
+      val middle = Card(Colour.Red, Number.eight)
       
-      hand.moeglich(mitte) shouldBe true
+      hand.possible(middle) shouldBe true
     }
 
     "allow playing a card if the value (Zahl) matches" in {
-      val hand = new Hand(List(Karte(Farbe.Blau, Zahl.acht)))
-      val mitte = Karte(Farbe.Rot, Zahl.acht)
+      val hand = new Hand(List(Card(Colour.Blue, Number.eight)))
+      val middle = Card(Colour.Red, Number.eight)
       
-      hand.moeglich(mitte) shouldBe true
+      hand.possible(middle) shouldBe true
     }
 
     "allow playing a card if it is a black (wild) card" in {
-      val hand = new Hand(List(Karte(Farbe.Schwarz, Zahl.plus4)))
-      val mitte = Karte(Farbe.Gruen, Zahl.fuenf)
+      val hand = new Hand(List(Card(Colour.Black, Number.plus4)))
+      val middle = Card(Colour.Green, Number.five)
       
-      hand.moeglich(mitte) shouldBe true
+      hand.possible(middle) shouldBe true
     }
 
     "not allow playing a card if neither color nor value matches" in {
-      val hand = new Hand(List(Karte(Farbe.Blau, Zahl.drei)))
-      val mitte = Karte(Farbe.Rot, Zahl.acht)
+      val hand = new Hand(List(Card(Colour.Blue, Number.three)))
+      val middle = Card(Colour.Red, Number.eight)
       
-      hand.moeglich(mitte) shouldBe false
+      hand.possible(middle) shouldBe false
     }
 
     "return false for moeglich if the hand is empty" in {
       val hand = new Hand(List.empty)
-      val mitte = Karte(Farbe.Rot, Zahl.acht)
+      val middle = Card(Colour.Red, Number.eight)
       
-      hand.moeglich(mitte) shouldBe false
+      hand.possible(middle) shouldBe false
     }
   }
 }
