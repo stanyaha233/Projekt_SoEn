e20f36d616a25e4b4bfc00391021124a8936679f
chrncln-create <nicolina.hoffmann@gmail.com>
2026-04-23 18:29:46 +0200

    Update UnoLogic.scala

--- src/main/scala/UnoLogic.scala
+++ src/main/scala/UnoLogic.scala
@@ -1,54 +1,53 @@
 package uno
 
-import uno.{Karte, Farbe, Zahl}
+import uno.{Card, Colour, Number}
 
 
-object UnoSpiel extends App { // Der Name des Objekts ist frei wählbar
-    // Hier kommt dein Code rein
-    var hand = KarteZiehen.anfangskarten(new Hand(List.empty[Karte]))
-    var gegnerhand = KarteZiehen.anfangskarten(new Hand(List.empty[Karte]))
-    var mitte = KarteZiehen.ziehen()
+object UnoSpiel extends App {
+    var hand = Draw.beginningHand(new Hand(List.empty[Card]))
+    var opponentCard = Draw.beginningHand(new Hand(List.empty[Card]))
+    var middle = Draw.draw()
     var turn: Boolean = true // true = Spieler, false = Gegner
     println("Uno")
-    while (hand.anzahl > 0 && gegnerhand.anzahl > 0) {
+    while (hand.count > 0 && opponentCard.count > 0) {
         println(" ")
-        println("Stapel: " + mitte.farbe + " " + mitte.wert)
+        println("Stapel: " + middle.colour + " " + middle.value)
         println(" ")
-        println("Gegner Hand Anzahl: " + gegnerhand.anzahl)
+        println("Gegner Hand Anzahl: " + opponentCard.count)
         println(" ")
-        println("Möglichkeit zu einem Zug: " + hand.moeglich(mitte))
+        println("Möglichkeit zu einem Zug: " + hand.possible(middle))
         println(" ")
         if (turn) {
-            if (mitte.wert == Zahl.plus2) {
+            if (middle.value == Number.plus2) {
                 println("Du musst 2 Karten ziehen!")
-                hand = hand.add(KarteZiehen.ziehen()).add(KarteZiehen.ziehen())
+                hand = hand.add(Draw.draw()).add(Draw.draw())
                 turn = false
             }
-            else if (mitte.wert == Zahl.plus4) {
+            else if (middle.value == Number.plus4) {
                 println("Du musst 4 Karten ziehen!")
-                hand = hand.add(KarteZiehen.ziehen()).add(KarteZiehen.ziehen()).add(KarteZiehen.ziehen()).add(KarteZiehen.ziehen())
+                hand = hand.add(Draw.draw()).add(Draw.draw()).add(Draw.draw()).add(Draw.draw())
                 turn = false
             }
-            else if (mitte.wert == Zahl.Skip) {
+            else if (middle.value == Number.Skip) {
                 println("Der Gegner überspringt deinen Zug!")
                 turn = false
-            } else if (mitte.wert == Zahl.Wechsel) {
+            } else if (middle.value == Number.directionChange) {
                 println("Richtungswechsel! Du darfst nochmal!")
                 turn = true
             }
             println(" ")
-            if(hand.moeglich(mitte)) {
+            if(hand.possible(middle)) {
                 println("Deine Hand:")
-        println(hand.karten.map(k => s"${k.farbe} ${k.wert}").mkString(", "))
+        println(hand.cards.map(k => s"${k.colour} ${k.value}").mkString(", "))
         println(" ")
             val input = scala.io.StdIn.readLine("Welche Karte möchtest du legen? (Farbe Nummer)")
             val Array(farbeInput, wertInput) = input.split(" ")
-            val gewaehlteFarbe = Farbe.withName(farbeInput)
-            val gewaehlterWert = Zahl.withName(wertInput)
-            if (gewaehlterWert == mitte.wert || gewaehlteFarbe == mitte.farbe || gewaehlteFarbe == Farbe.Schwarz) {
-                println(s"Du hast die Karte ${gewaehlteFarbe} ${gewaehlterWert} gelegt.")
-                hand = new Hand(hand.karten.filterNot(k => k.farbe == gewaehlteFarbe && k.wert == gewaehlterWert))
-                mitte = Karte(gewaehlteFarbe, gewaehlterWert)
+            val chosenColour = Colour.withName(farbeInput)
+            val chosenValue = Number.withName(wertInput)
+            if (chosenValue == middle.value || chosenColour == middle.colour || chosenColour == Colour.Black) {
+                println(s"Du hast die Karte ${chosenColour} ${chosenValue} gelegt.")
+                hand = new Hand(hand.cards.filterNot(k => k.colour == chosenColour && k.value == chosenValue))
+                middle = Card(chosenColour, chosenValue)
                 turn = false
             } else {
             println("Ungültiger Zug. Bitte versuche es erneut.")
@@ -56,45 +55,45 @@ object UnoSpiel extends App { // Der Name des Objekts ist frei wählbar
             }
             } else {
             println("Du musst eine Karte ziehen.")
-            hand = hand.add(KarteZiehen.ziehen())
+            hand = hand.add(Draw.draw())
             turn = false
             }
-            if (hand.anzahl == 0) {
+            if (hand.count == 0) {
             println("Herzlichen Glückwunsch! Du hast gewonnen!")
             }
         }
          else {
-            if (mitte.wert == Zahl.plus2) {
+            if (middle.value == Number.plus2) {
                 println("Du musst 2 Karten ziehen!")
-                hand = hand.add(KarteZiehen.ziehen()).add(KarteZiehen.ziehen())
+                hand = hand.add(Draw.draw()).add(Draw.draw())
                 turn = true
             }
-            else if (mitte.wert == Zahl.plus4) {
+            else if (middle.value == Number.plus4) {
                 println("Du musst 4 Karten ziehen!")
-                hand = hand.add(KarteZiehen.ziehen()).add(KarteZiehen.ziehen()).add(KarteZiehen.ziehen()).add(KarteZiehen.ziehen())
+                hand = hand.add(Draw.draw()).add(Draw.draw()).add(Draw.draw()).add(Draw.draw())
                 turn = true
             }
-            else if (mitte.wert == Zahl.Skip) {
+            else if (middle.value == Number.Skip) {
                 println("Der Gegner überspringt deinen Zug!")
                 turn = true
-            } else if (mitte.wert == Zahl.Wechsel) {
+            } else if (middle.value == Number.directionChange) {
                 println("Richtungswechsel! Du darfst nochmal!")
                 turn = false
             }
-            if(gegnerhand.moeglich(mitte)) {
-                val gewaehlteKarte = gegnerhand.karten.find(k => k.farbe == mitte.farbe || k.wert == mitte.wert || k.farbe == Farbe.Schwarz)
-                if (gewaehlteKarte != None) {
-                    val karte = gewaehlteKarte.head
-                    gegnerhand = new Hand(gegnerhand.karten.filterNot(k => k.farbe == karte.farbe && k.wert == karte.wert))
-                    mitte = karte
-                    println(s"Der Gegner hat die Karte ${karte.farbe} ${karte.wert} gelegt.")
+            if(opponentCard.possible(middle)) {
+                val chosenCard = opponentCard.cards.find(k => k.colour == middle.colour || k.value == middle.value || k.colour == Colour.Black)
+                if (chosenCard != None) {
+                    val card = chosenCard.head
+                    opponentCard = new Hand(opponentCard.cards.filterNot(k => k.colour == card.colour && k.value == card.value))
+                    middle = card
+                    println(s"Der Gegner hat die Karte ${card.colour} ${card.value} gelegt.")
                 }} else {
                 println("Der Gegner muss eine Karte ziehen.")
-                gegnerhand = gegnerhand.add(KarteZiehen.ziehen())
+                opponentCard = opponentCard.add(Draw.draw())
             }
             turn = true
             println(" ")
-            if (gegnerhand.anzahl == 0) {
+            if (opponentCard.count == 0) {
                 println("Der Gegner hat gewonnen! Viel Glück beim nächsten Mal!") 
             }
         }
