2660c3266ec73bd698d152ea1c0ebd8108ee95fd
chrncln-create <nicolina.hoffmann@gmail.com>
2026-04-23 18:29:49 +0200

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,34 +1,34 @@
 package uno
 
-case class Karte(farbe: Farbe.Value, wert: Zahl.Value)
+case class Card(colour: Colour.Value, value: Number.Value)
 
 
 def main(): Unit = {
 
-  val mitte = Karte(Farbe.Rot, Zahl.sieben)
+  val middle = Card(Colour.Red, Number.seven)
 
-  val karte1 = Karte(Farbe.Blau, Zahl.drei)
-  val karte2 = Karte(Farbe.Gruen, Zahl.Skip)
-  val karte3 = Karte(Farbe.Rot,Zahl.zwei)
-  val karte4 = Karte(Farbe.Schwarz, Zahl.wahl)
-  val karte5 = Karte(Farbe.Schwarz,Zahl.plus4)
-  val karte6 = Karte(Farbe.Blau, Zahl.plus2)
-  val karte7 = Karte(Farbe.Gruen, Zahl.Wechsel)
+  val card1 = Card(Colour.Blue, Number.three)
+  val card2 = Card(Colour.Green, Number.Skip)
+  val card3 = Card(Colour.Red,Number.two)
+  val card4 = Card(Colour.Black, Number.choice)
+  val card5 = Card(Colour.Black,Number.plus4)
+  val card6 = Card(Colour.Blue, Number.plus2)
+  val card7 = Card(Colour.Green, Number.directionChange)
 
-  val hand = List(karte1, karte2, karte3)
-  val handGegner = List(karte6, karte5, karte4, karte7)
+  val hand = List(card1, card2, card3)
+  val handGegner = List(card6, card5, card4, card7)
 
   println("Uno")
-  println("Stapel: " + mitte.farbe + " " + mitte.wert)
+  println("Stapel: " + middle.colour + " " + middle.value)
   println(" ")
    println("Gegner Hand Anzahl:" + handGegner.size)
 
   println("Deine Hand:")
-  println("Karte 1: " + karte1.farbe + " " + karte1.wert)
-  println("Karte 2: " + karte2.farbe + " " + karte2.wert)
-  println("Karte 3: " + karte3.farbe + " " + karte3.wert)
+  println("Karte 1: " + card1.colour + " " + card1.value)
+  println("Karte 2: " + card2.colour + " " + card2.value)
+  println("Karte 3: " + card3.colour + " " + card3.value)
 
-  val moeglich = hand.exists(k => k.farbe == mitte.farbe || k.wert == mitte.wert || k.farbe == Farbe.Schwarz)
+  val moeglich = hand.exists(k => k.colour == middle.colour || k.value == middle.value || k.colour == Colour.Black)
 
   println(" ")
   println("Möglichkeit zu einem Zug: " + moeglich)
