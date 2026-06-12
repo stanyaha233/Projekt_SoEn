e2dbc68fa5a73cd901d8b8236821fd5be765f7f5
Hanna stanyak <132809702+stanyaha233@users.noreply.github.com>
2026-06-12 09:36:22 +0200

    Update BotStrategy.scala

--- src/main/scala/uno/controller/BotStrategy.scala
+++ src/main/scala/uno/controller/BotStrategy.scala
@@ -10,3 +10,11 @@ class FirstPossibleStrategy extends BotStrategy {
   override def chooseCard(hand: Hand, activeColour: Colour.Value, pileValue: Number.Value): Option[Card] =
     hand.cards.find(c => c.colour == activeColour || c.value == pileValue || c.colour == Colour.Black)
 }
+
+class BlackFirstStrategy extends BotStrategy {
+  override def chooseCard(hand: Hand, activeColour: Colour.Value, pileValue: Number.Value): Option[Card] =
+    hand.cards.find(c => c.colour == Colour.Black) match {
+      case Some(blackCard) => Some(blackCard)
+      case None => hand.cards.find(c => c.colour == activeColour || c.value == pileValue)
+    }
+}
