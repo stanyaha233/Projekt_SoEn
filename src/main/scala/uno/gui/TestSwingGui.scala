259792142d87cff8d835fccd89bb23ed70c2e3f1
chrncln-create <nicolina.hoffmann@gmail.com>
2026-06-12 09:33:47 +0200

    Delete TestSwingGui.scala

--- src/main/scala/uno/gui/TestSwingGui.scala
+++
@@ -1,8 +0,0 @@
-package uno.gui
-
-import uno.controller.UnoLogic
-import uno.model._
-
-class TestSwingGui(controller: UnoLogic) extends SwingGui(controller) {
-  override def askForColour(): Colour.Value = Colour.Red
-}
