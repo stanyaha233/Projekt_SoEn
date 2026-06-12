23cf31c38746da68c26dabfebed7e996de22a797
chrncln-create <nicolina.hoffmann@gmail.com>
2026-06-12 09:32:01 +0200

    Update BotStrategy.scala

--- src/main/scala/uno/controller/BotStrategy.scala
+++ src/main/scala/uno/controller/BotStrategy.scala
@@ -2,11 +2,11 @@ package uno.controller
 
 import uno.model._
 
-trait CpuStrategy {
+trait BotStrategy {
   def chooseCard(hand: Hand, activeColour: Colour.Value, pileValue: Number.Value): Option[Card]
 }
 
-class FirstPossibleStrategy extends CpuStrategy {
+class FirstPossibleStrategy extends BotStrategy {
   override def chooseCard(hand: Hand, activeColour: Colour.Value, pileValue: Number.Value): Option[Card] =
     hand.cards.find(c => c.colour == activeColour || c.value == pileValue || c.colour == Colour.Black)
 }
