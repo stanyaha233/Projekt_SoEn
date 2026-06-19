0aa584063ec3e9e68a8b65363fd583b6b03632fa
chrncln-create <nicolina.hoffmann@gmail.com>
2026-06-19 12:00:54 +0200

    Update PlaceCardCommand.scala

--- src/test/scala/controller/PlaceCardCommand.scala
+++ src/test/scala/controller/PlaceCardCommand.scala
@@ -9,7 +9,7 @@ import uno.model.components.GameState
 class PlaceCardCommandSpec extends AnyWordSpec with Matchers {
   "A PlaceCardCommand" should {
 
-    // Test-Zustand: Wir wählen Farbe Blau, um zum pile zu passen
+    // Test-Zustand
     val initialState = GameState(
       playerHand = Hand(List(Card(Colour.Blue, Number.one))),
       cpuHand = Hand(List()),
